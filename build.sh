62447c3256587f4b923c257c990808cccc83a8e0
Matthias Ableidinger <ableimat@gmx.at>
2015-11-09 23:12:47 +0100

    added first version of backend-server + buildscript

--- build.sh
+++ build.sh
@@ -2,4 +2,17 @@
 
 mkdir out
 
+#www-data
 cp -rf www out/www
+
+#service-backend
+mkdir out/service
+cp -rf service/web/WEB-INF out/service
+mkdir out/service/WEB-INF/classes
+javac -cp "service/lib/*" -d out/service/WEB-INF/classes service/src/com/slothyx/*.java
+mkdir out/service/WEB-INF/lib
+cp service/lib/JSON.jar out/service/WEB-INF/lib
+
+jar cvf out/service.war -C out/service .
+
+cp out/service.war ../service.war
---
+++ service/src/com/slothyx/TestServlet.java
@@ -0,0 +1,31 @@
+package com.slothyx;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try{
+			JSONObject json = new JSONObject();
+			json.put("url",request.getRequestURL());
+			json.put("uri",request.getRequestURI());
+			json.put("contextPath",request.getContextPath());
+			writeJSONResponse(response, json.toString());
+		}catch (JSONException ignored){
+
+		}
+	}
+
+	private void writeJSONResponse(HttpServletResponse response, String json) throws IOException {
+		response.getWriter().println(json);
+		response.setContentType("application/json");
+	}
+}
